0ee80046401394c1d3dd8379154821fe502812d9
jslate9 <105230774+jslate9@users.noreply.github.com>
2022-05-09 13:40:17 -0400

    Add commentary

--- scripts/macos/LMNTRIX-Install.sh
+++ scripts/macos/LMNTRIX-Install.sh
@@ -11,7 +11,12 @@ cd /tmp/$TempFolder
 #Download Required Files
 #CFG File
 curl -LJO https://github.com/jslate9/pkgs/raw/main/installers/LMNTRIX/macos/SensorMacOSInstaller-RIVALRY_Detect_3.60.2.cfg
+# Capture name of Download File and report it
+DownloadFile="$(ls)"
+echo "Downloaded $DownloadFile to /tmp/$TempFolder"
 #INSTALL File
 curl -LJO https://github.com/jslate9/pkgs/raw/main/installers/LMNTRIX/macos/SensorMacOSInstaller-RIVALRY_Detect_3.60.2
-
+# Capture name of Download File and report it
+DownloadFile="$(ls)"
+echo "Downloaded $DownloadFile to /tmp/$TempFolder"
 
