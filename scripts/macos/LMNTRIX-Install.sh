f3b134e34122d8704fdcc52c1be2e7a1ef474703
jslate9 <105230774+jslate9@users.noreply.github.com>
2022-05-09 13:17:26 -0400

    Added new Download paths for script

--- scripts/macos/LMNTRIX-Install.sh
+++ scripts/macos/LMNTRIX-Install.sh
@@ -10,8 +10,8 @@ cd /tmp/$TempFolder
 
 #Download Required Files
 #CFG File
-curl -s -O /tmp/test/SensorMacOSInstaller-RIVALRY_Detect_3.60.2.cfg "https://00e9e64bac57476399393b29c72c4bdf74f2b85a72bdeef675-apidata.googleusercontent.com/download/drive/v3/files/1noK3jWi7OGZcXbdKkOz-OT80XGmyE-HZ?qk=AD5uMEtoDXpLUqD56DY4gfN1ZOlcyV5B9sRE6_qL468_n_cUAMq_gMWzheejbb461V31EdmDYqF_9cbHYdl5n4voCnzSn0qixJI5-Mc749jIgODnWDdmDljeSBArj1trmcDNIKevbnZ5wnyA5YbNkKzEIGfl1101psS4X4RkZbbAglsmsbUuPuidgqdAOIHPEY-slz6UaNi28VZhwM4BW4w_8vvrNRB3HA"
+curl -s -O ./SensorMacOSInstaller-RIVALRY_Detect_3.60.2.cfg 'https://github.com/jslate9/pkgs/raw/main/installers/LMNTRIX/macos/SensorMacOSInstaller-RIVALRY_Detect_3.60.2.cfg'
 #INSTALL File
-curl -s -O 'https://drive.google.com/uc?export=download&id=1idTEhJg56I64ZNVkZJJlUO79TkN2SZ6o'
+curl -s -O ./SensorMacOSInstaller-RIVALRY_Detect_3.60.2 'https://github.com/jslate9/pkgs/raw/main/installers/LMNTRIX/macos/SensorMacOSInstaller-RIVALRY_Detect_3.60.2'
 
 
