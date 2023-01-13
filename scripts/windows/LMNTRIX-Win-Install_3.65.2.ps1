167cf549559384114e3fc75d9954da2be7756c85
jslate9 <105230774+jslate9@users.noreply.github.com>
2023-01-13 13:41:28 -0500

    Update LMNTRIX-Win-Install_3.65.2.ps1

--- scripts/windows/LMNTRIX-Win-Install_3.65.2.ps1
+++ scripts/windows/LMNTRIX-Win-Install_3.65.2.ps1
@@ -1,24 +1,23 @@
 #Set variables
-DATE=$(date '+%Y-%m-%d-%H-%M-%S')
-TempFolder="Download-$DATE"
+$Date = (Get-Date).tostring("yy-MM-dd-HH-mm-ss") 
+$folderPath = "C:\Windows\Temp"
+$exe = "LMNTRIX_Endgame_3.65.2.exe"
+$cfg = "LMNTRIX_Endgame_3.65.2.cfg"
 
-#Make Working folder in temp
-mkdir /tmp/$TempFolder
+
+#Make Working folder in temp && Capture Folder Name
+$foldername = (New-Item -ItemType Directory -Path $folderPath -Name LMNTRIX_$Date).name
 
 # Navigate to Temp Folder
-cd /tmp/$TempFolder
+cd $folderpath\$foldername
 
 #Download Files
 #CFG File
-curl -LJO https://github.com/jslate9/pkgs/raw/main/installers/LMNTRIX/macos/SensorMacOSInstaller-RIVALRY_Detect_3.62.5.cfg
+$DownloadedCFGFile = (wget "https://github.com/jslate9/pkgs/raw/main/installers/LMNTRIX/macos/SensorMacOSInstaller-RIVALRY_Detect_3.62.5.cfg" -outfile $folderpath\$foldername\$cfg).path
+
 # Capture name of Download File and report it
-DownloadedCFGFile="$(ls)"
-
-
-#Install File
-
-
-
+#$DownloadedCFGFile = $folderpath\$foldername\$cfg
+echo "CFG File downloaded to "$DownloadedCFGFile
 
 #Install App
 #<driver>.exe -c <driver>.cfg -k 1E500BF92DFCF6F7F1AB -d false -l C:\Windows\Temp\Respond_Installer.txt
