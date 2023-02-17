8676a2d6afb0a13bde0afcb56235c0bf0ed8c099
jslate9 <105230774+jslate9@users.noreply.github.com>
2023-02-17 15:14:05 -0500

    Update LMNTRIX-Install.sh

--- scripts/macos/LMNTRIX-Install.sh
+++ scripts/macos/LMNTRIX-Install.sh
@@ -1,26 +1,41 @@
 #!/bin/bash
 #This Script is for installation of the agent
 
+endgameStatus=$(pgrep esensor)
 #Create Temp Folder
 DATE=$(date '+%Y-%m-%d-%H-%M-%S')
 TempFolder="Download-$DATE"
-mkdir /tmp/$TempFolder
-# Navigate to Temp Folder
-cd /tmp/$TempFolder
 
-#Download Required Files
-#CFG File
-curl -LJO https://github.com/jslate9/pkgs/raw/main/installers/LMNTRIX/macos/SensorMacOSInstaller-RIVALRY_Detect_3.60.2.cfg
-# Capture name of Download File and report it
-DownloadedCFGFile="$(ls)"
-echo "Downloaded $DownloadedCFGFile to /tmp/$TempFolder"
+if [[ $endgameStatus ]]; then
+echo "endgame is running/installed, not downloading or installing"
+else
+    mkdir /tmp/$TempFolder
+    # Navigate to Temp Folder
+    cd /tmp/$TempFolder
 
-#INSTALL File
-curl -LJO https://github.com/jslate9/pkgs/raw/main/installers/LMNTRIX/macos/SensorMacOSInstaller-RIVALRY_Detect_3.60.2
-# Capture name of Download File and report it
-DownloadedINSTALLFile="$(ls)"
-echo "Downloaded $DownloadedINSTALLFile to /tmp/$TempFolder"
+    #Download Required Files
+    #CFG File
+    curl -LJO https://github.com/jslate9/pkgs/raw/main/installers/LMNTRIX/macos/SensorMacOSInstaller-RIVALRY_Detect_3.62.5.cfg
+    # Capture name of Download File and report it
+    DownloadedCFGFile="$(ls)"
+    echo "Downloaded $DownloadedCFGFile to /tmp/$TempFolder"
+    #INSTALL File
+    curl -LJO https://github.com/jslate9/pkgs/raw/main/installers/LMNTRIX/macos/SensorMacOSInstaller-RIVALRY_Detect_3.62.5
+    # Capture name of Download File and report it
+    DownloadedINSTALLFile="$(ls)"
+    echo "Downloaded $DownloadedINSTALLFile to /tmp/$TempFolder"
+    chmod +x $DownloadedINSTALLFile
 
-
-chmod +x $DownloadedINSTALLFile
-./$DownloadedINSTALLFile -c $DownloadedCFGFile -k 1E500BF92DFCF6F7F1AB -d false -l install.log
+    echo "stating install"
+    ./$DownloadedINSTALLFile -c $DownloadedCFGFile -k 1E500BF92DFCF6F7F1AB -d false -l install.log
+    if [ $? -eq 0 ]; then
+        echo "Installer ran successfully"
+        Sleep 30
+        endgameStatus=$(pgrep esensor)
+        if [[ $endgameStatus ]]; then
+            echo "endgame started successfully after install"
+        else
+            echo "endgame failed to start after install"
+        fi
+    fi
+fi
